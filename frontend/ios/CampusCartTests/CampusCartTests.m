939b37a75dc082d1b080f58e6c3a3b42417d85ea
Osita Miles <siscomilesinfo@gmail.com>
2025-08-08 06:28:38 +0000

    Rename test class from front_endTests to CampusCartTests for consistency

--- frontend/ios/CampusCartTests/CampusCartTests.m
+++ frontend/ios/CampusCartTests/CampusCartTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface front_endTests : XCTestCase
+@interface CampusCartTests : XCTestCase
 
 @end
 
-@implementation front_endTests
+@implementation CampusCartTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
